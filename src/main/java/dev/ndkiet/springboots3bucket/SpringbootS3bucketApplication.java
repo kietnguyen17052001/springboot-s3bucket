f2c259403c7c29a0b044f48306750ae0d239b442
ngdangkiet <kietnguyen17052001@gmail.com>
2023-02-26 15:05:51 +0700

    init project springboot-s3

---
+++ src/main/java/dev/ndkiet/springboots3bucket/SpringbootS3bucketApplication.java
@@ -0,0 +1,13 @@
+package dev.ndkiet.springboots3bucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootS3bucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootS3bucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ndkiet/springboots3bucket/SpringbootS3bucketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ndkiet.springboots3bucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootS3bucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
