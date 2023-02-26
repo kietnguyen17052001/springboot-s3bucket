88c26634f7b4b7850ba47336019f8b25e5139485
ngdangkiet <kietnguyen17052001@gmail.com>
2023-02-26 22:32:32 +0700

    feat: service s3

---
+++ src/main/java/dev/ndkiet/springboots3bucket/controller/S3BucketController.java
@@ -0,0 +1,10 @@
+package dev.ndkiet.springboots3bucket.controller;
+
+/**
+ * @author ADMIN
+ * @created 26/02/2023 - 10:30 PM
+ * @project springboot-s3bucket
+ */
+
+public class S3BucketController {
+}
---
+++ src/main/java/dev/ndkiet/springboots3bucket/service/S3BucketService.java
@@ -0,0 +1,57 @@
+package dev.ndkiet.springboots3bucket.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @author ADMIN
+ * @created 26/02/2023 - 9:59 PM
+ * @project springboot-s3bucket
+ */
+
+@Service
+@RequiredArgsConstructor
+public class S3BucketService {
+    private final AmazonS3 s3Client;
+    @Value("${spring.s3.bucket-name}")
+    private String bucketName;
+
+    public void uploadFileToS3(MultipartFile multipartFile) throws IOException {
+        File file = transferMultipartFile2File(multipartFile);
+        String fileName = String.format("%d_%s", System.currentTimeMillis(), multipartFile.getOriginalFilename());
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, file));
+    }
+
+    public byte[] downloadFileFromS3(String fileName) {
+        S3Object s3Object = s3Client.getObject(bucketName, fileName);
+        // read content object
+        S3ObjectInputStream s3ObjectInputStream = s3Object.getObjectContent();
+        try {
+            return IOUtils.toByteArray(s3ObjectInputStream);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public void deleteFile(String fileName) {
+        s3Client.deleteObject(bucketName, fileName);
+    }
+
+    public File transferMultipartFile2File(MultipartFile multipartFile) throws IOException {
+        File convertedFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        multipartFile.transferTo(convertedFile);
+        return convertedFile;
+    }
+}
